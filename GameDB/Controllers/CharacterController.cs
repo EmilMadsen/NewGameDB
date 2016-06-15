5c9adbcb200808c8fc22e5e81e5ea5904f6d2c1e
Emil Madsen <Ostebob1234@gmail.com>
2016-06-15 16:29:07 +0200

    character CRUD updated. Indexed characters on Games details

--- GameDB/Controllers/CharacterController.cs
+++ GameDB/Controllers/CharacterController.cs
@@ -13,6 +13,11 @@ namespace GameDB.Controllers
         CharacterRepository CharRepo = new CharacterRepository();
         GameRepository GameRepo = new GameRepository();
 
+        public ActionResult Index()
+        {
+            return View(CharRepo.GetAll());
+        }
+
         [HttpGet]
         public ActionResult Create()
         {
@@ -36,6 +41,44 @@ namespace GameDB.Controllers
                 return RedirectToAction("Details", "Game", new { id = character.ParentGameID });
             }
             return View();
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+
+            return View(CharRepo.Find(id));
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            int CharID = CharRepo.Find(id).ParentGameID;
+            CharRepo.Delete(id);
+            return RedirectToAction("Details", "Game", new { id = CharID });
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(CharRepo.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Character character, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                if(image != null)
+                {
+                    character.ImageMimeType = image.ContentType;
+                    character.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(character.ImageData, 0, image.ContentLength);
+                }
+                CharRepo.InsertOrUpdate(character);
+                return RedirectToAction("Details", "Game", new { id = character.ParentGameID});
+            }
+            return View(character);
+           
         }
        
     }

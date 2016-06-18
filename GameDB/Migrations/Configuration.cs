db6600ea12e2fd0d33d7b943dd08a2dee2bc1ec9
Peter Raasthøj <Peter Raasthj>
2016-06-18 10:47:48 +0200

    seeding method for admin

--- GameDB/Migrations/Configuration.cs
+++ GameDB/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace GameDB.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -26,6 +29,25 @@ namespace GameDB.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "AppAdmin"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "AppAdmin", Email = "Admin@hotmail.com" };
+
+                manager.Create(user, "Kode1234.");
+                manager.AddToRole(user.Id, "Admin");
+            }
         }
     }
 }
